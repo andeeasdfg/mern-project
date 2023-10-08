bb12cb0d425f81bb4367485e611658b1ccc5df45
Adrianne Marie Malonzo <malonzoandee@gmail.com>
2023-10-08 23:29:22 +0800

    transfered git-ignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1 className="text-4xl mr-auto text-center font-mono">
+        Tailwind kineme
+      </h1>
+    </div>
+  );
 }
 
 export default App;
